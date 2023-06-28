a9a07f931e8912fe12189a6e1c418956739d4d37
김창현 <gusl4497@gmail.com>
2023-06-28 13:54:50 +0900

    login_page

--- common/urls.py
+++ common/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from common.views import register, registration_complete
-
+from . import views
 app_name = 'common'
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('register_complete/', registration_complete, name='registration_complete'),
+    path('login/',views.login ,name="login"), ## 로그인 url 
 ]

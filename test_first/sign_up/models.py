c9cabc59b58495b0764ba187ea7637a1bfd46a36
ChaeyoonLim <nalcy020819@naver.com>
2023-07-07 07:47:00 +0900

    강제 pull을 받았더니 이상하게 merge 되어 있다. 수정은 수작업으로 해야 할 듯.. 수정 후 다시 push 할 예정.

--- test_first/sign_up/models.py
+++
@@ -1,8 +0,0 @@
-from djongo import models
-
-class MyModel(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
-
-    class Meta:
-        db_table = 'my_model_collection'  # MongoDB 컬렉션 이름 설정
